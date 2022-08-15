fd8a2a831e6b85a70d619af0808d10a779cad881
Brandon <brandon.r.young@outlook.com>
2022-08-15 19:03:28 -0400

    fit ship rotating bug

--- src/modules/DOM/modal.js
+++ src/modules/DOM/modal.js
@@ -5,55 +5,76 @@ const initModal = () => {
     axisBtn.addEventListener('click', () => {
         if (axisBtn.textContent.charAt(6) === 'X') {
             axisBtn.textContent = 'AXIS: Y';
-            modalHoverEvents('vert');
+            direction = 'vert';
         } else {
             axisBtn.textContent = 'AXIS: X';
-            modalHoverEvents('hori');
+            direction = 'hori';
         }
     })
 }
 
-
+let direction = 'hori';
 
 function modalHoverEvents (dir) {
     const cells = document.querySelectorAll('.modalCell');
 
     cells.forEach((cell) => {
-        const thisX = parseInt(cell.getAttribute('data-x'));
-        const thisY = parseInt(cell.getAttribute('data-y'));
-        let oneOver;
-        let twoOver;
-        let threeOver;
-        let fourOver
-        if (dir === 'vert') {
-            oneOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 1}"]`);
-            twoOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 2}"]`);
-            threeOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 3}"]`);
-            fourOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 4}"]`);
-        } else {
-            oneOver = document.querySelector(`.modalCell[data-x="${thisX + 1}"][data-y="${thisY}"]`);
-            twoOver = document.querySelector(`.modalCell[data-x="${thisX + 2}"][data-y="${thisY}"]`);
-            threeOver = document.querySelector(`.modalCell[data-x="${thisX + 3}"][data-y="${thisY}"]`);
-            fourOver = document.querySelector(`.modalCell[data-x="${thisX + 4}"][data-y="${thisY}"]`);
-        }
-
-        cell.addEventListener('mouseenter', () => {
-            if (!oneOver || !twoOver || !threeOver || !fourOver) {cell.classList.add('placeError'); return}
-            cell.classList.add('placeHover');
-            oneOver.classList.add('placeHover');
-            twoOver.classList.add('placeHover');
-            threeOver.classList.add('placeHover');
-            fourOver.classList.add('placeHover');
-        });
-        cell.addEventListener('mouseleave', () => {
-            if (!oneOver || !twoOver || !threeOver || !fourOver) {cell.classList.remove('placeError'); return}
-            cell.classList.remove('placeHover');
-            oneOver.classList.remove('placeHover');
-            twoOver.classList.remove('placeHover');
-            threeOver.classList.remove('placeHover');
-            fourOver.classList.remove('placeHover');
-        })
+        cell.addEventListener('mouseenter', () => addClassHover(cell));
+        cell.addEventListener('mouseleave', () => removeClassHover(cell));
     })
+}
+
+function addClassHover (cell) {
+    const thisX = parseInt(cell.getAttribute('data-x'));
+    const thisY = parseInt(cell.getAttribute('data-y'));
+    let oneOver;
+    let twoOver;
+    let threeOver;
+    let fourOver;
+    if (direction === 'vert') {
+        oneOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 1}"]`);
+        twoOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 2}"]`);
+        threeOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 3}"]`);
+        fourOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 4}"]`);
+    } else {
+        oneOver = document.querySelector(`.modalCell[data-x="${thisX + 1}"][data-y="${thisY}"]`);
+        twoOver = document.querySelector(`.modalCell[data-x="${thisX + 2}"][data-y="${thisY}"]`);
+        threeOver = document.querySelector(`.modalCell[data-x="${thisX + 3}"][data-y="${thisY}"]`);
+        fourOver = document.querySelector(`.modalCell[data-x="${thisX + 4}"][data-y="${thisY}"]`);
+    }
+
+    if (!oneOver || !twoOver || !threeOver || !fourOver) {cell.classList.add('placeError'); return}
+    cell.classList.add('placeHover');
+    oneOver.classList.add('placeHover');
+    twoOver.classList.add('placeHover');
+    threeOver.classList.add('placeHover');
+    fourOver.classList.add('placeHover');
+}
+
+function removeClassHover (cell) {
+    const thisX = parseInt(cell.getAttribute('data-x'));
+    const thisY = parseInt(cell.getAttribute('data-y'));
+    let oneOver;
+    let twoOver;
+    let threeOver;
+    let fourOver
+    if (direction === 'vert') {
+        oneOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 1}"]`);
+        twoOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 2}"]`);
+        threeOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 3}"]`);
+        fourOver = document.querySelector(`.modalCell[data-x="${thisX}"][data-y="${thisY + 4}"]`);
+    } else {
+        oneOver = document.querySelector(`.modalCell[data-x="${thisX + 1}"][data-y="${thisY}"]`);
+        twoOver = document.querySelector(`.modalCell[data-x="${thisX + 2}"][data-y="${thisY}"]`);
+        threeOver = document.querySelector(`.modalCell[data-x="${thisX + 3}"][data-y="${thisY}"]`);
+        fourOver = document.querySelector(`.modalCell[data-x="${thisX + 4}"][data-y="${thisY}"]`);
+    }
+    if (!oneOver || !twoOver || !threeOver || !fourOver) {cell.classList.remove('placeError'); return}
+    cell.classList.remove('placeHover');
+    oneOver.classList.remove('placeHover');
+    twoOver.classList.remove('placeHover');
+    threeOver.classList.remove('placeHover');
+    fourOver.classList.remove('placeHover');
 }
 
 export default initModal
