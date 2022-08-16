706d7097a3e6739650178eee7f4f9b3da56a85bc
Brandon <brandon.r.young@outlook.com>
2022-08-15 23:28:09 -0400

    continue work on modal

--- src/modules/DOM/modal.js
+++ src/modules/DOM/modal.js
@@ -14,17 +14,20 @@ const initModal = () => {
 }
 
 let direction = 'hori';
+let ship = 1;
 
-function modalHoverEvents (dir) {
+function modalHoverEvents () {
     const cells = document.querySelectorAll('.modalCell');
 
     cells.forEach((cell) => {
         cell.addEventListener('mouseenter', () => addClassHover(cell));
         cell.addEventListener('mouseleave', () => removeClassHover(cell));
+        cell.addEventListener('click', () => placeShipOnModal(cell));
     })
 }
 
 function addClassHover (cell) {
+    if (cell.firstChild) {cell.classList.add('alreadyPlacedError'); return}
     const thisX = parseInt(cell.getAttribute('data-x'));
     const thisY = parseInt(cell.getAttribute('data-y'));
     let oneOver;
@@ -75,6 +78,66 @@ function removeClassHover (cell) {
     twoOver.classList.remove('placeHover');
     threeOver.classList.remove('placeHover');
     fourOver.classList.remove('placeHover');
+    if (cell.firstChild) cell.classList.remove('alreadyPlacedError')
+}
+
+
+function placeShipOnModal (cell) {
+    if (ship === 1) {
+        if (cell.firstChild) return;
+        const carrier = document.createElement('img');
+        carrier.classList.add('carrier');
+        if (direction === 'vert') carrier.classList.add('vertical');
+        carrier.src = './resources/carrier.png';
+        cell.appendChild(carrier);
+        removeClassHover(cell);
+        ship++;
+        return;
+    }
+    if (ship === 2) {
+        if (cell.firstChild) return;
+        const battleship = document.createElement('img');
+        battleship.classList.add('battleship');
+        if (direction === 'vert') battleship.classList.add('vertical');
+        battleship.src = './resources/battleship.png';
+        cell.appendChild(battleship);
+        removeClassHover(cell);
+        ship++;
+        return;
+    }
+    if (ship === 3) {
+        if (cell.firstChild) return;
+        const cruiser = document.createElement('img');
+        cruiser.classList.add('cruiser');
+        if (direction === 'vert') cruiser.classList.add('vertical');
+        cruiser.src = './resources/cruiser.png';
+        cell.appendChild(cruiser);
+        removeClassHover(cell);
+        ship++;
+        return;
+    }
+    if (ship === 4) {
+        if (cell.firstChild) return;
+        const submarine = document.createElement('img');
+        submarine.classList.add('submarine');
+        if (direction === 'vert') submarine.classList.add('vertical');
+        submarine.src = './resources/submarine.png';
+        cell.appendChild(submarine);
+        removeClassHover(cell);
+        ship++;
+        return;
+    }
+    if (ship === 5) {
+        if (cell.firstChild) return;
+        const destroyer = document.createElement('img');
+        destroyer.classList.add('destroyer');
+        if (direction === 'vert') destroyer.classList.add('vertical');
+        destroyer.src = './resources/destroyer.png';
+        cell.appendChild(destroyer);
+        removeClassHover(cell);
+        ship++;
+        return;
+    }
 }
 
 export default initModal
