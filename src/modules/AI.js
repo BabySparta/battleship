57e4b19c05b82d08a4655331f35dfaf820306025
Brandon <brandon.r.young@outlook.com>
2022-08-18 19:11:13 -0400

    work more on AI

--- src/modules/AI.js
+++ src/modules/AI.js
@@ -16,8 +16,8 @@ function AImove (gameboard) {
 
     let firstHit = validSpots[0];
     console.log(firstHit);
-    /* const smartAttack = findShips(firstHit, gameboard);
-    if (smartAttack) return smartAttack; */
+    const smartAttack = findShips(firstHit, gameboard, validSpots);
+    if (smartAttack) {console.log(firstHit); return smartAttack;}
     if (firstHit[1] + 1 < 10 && !gameboard.board[firstHit[1] + 1][firstHit[0]].isHit) {gameboard.recieveAttack(firstHit[0], firstHit[1] + 1); return [firstHit[0], firstHit[1] + 1]}
     else if (firstHit[0] + 1 < 10 && !gameboard.board[firstHit[1]][firstHit[0] + 1].isHit) {gameboard.recieveAttack(firstHit[0] + 1, firstHit[1]); return [firstHit[0] + 1, firstHit[1]]}
     else if (firstHit[1] - 1 >= 0 && !gameboard.board[firstHit[1] - 1][firstHit[0]].isHit) {gameboard.recieveAttack(firstHit[0], firstHit[1] - 1); return [firstHit[0], firstHit[1] - 1]}
@@ -34,31 +34,39 @@ const hasOpenSpace = (shot, gameboard) => {
     return false;
 }
 
-const findShips = (shot, gameboard) => {
+const findShips = (shot, gameboard, validSpots) => {
     // Vertical Ship
-/*     if ((gameboard.board[shot[1]][shot[0]].isHit && gameboard.board[shot[1]][shot[0]].hasShip) &&
-        (gameboard.board[shot[1] + 1][shot[0]].isHit && gameboard.board[shot[1] + 1][shot[0]].hasShip) && 
-        (!gameboard.board[shot[1] + 2][shot[0]].isHit)) {
-            shot[1] = shot[1] + 2;
+    if (gameboard.board[shot[1] - 1][shot[0]].isHit && gameboard.board[shot[1] + 1][shot[0]].isHit) {
+        const index = validSpots.indexOf(shot);
+        let newValid = validSpots.splice(index, 1);
+        shot = newValid[0];
     }
-    if ((gameboard.board[shot[1]][shot[0]].isHit && gameboard.board[shot[1]][shot[0]].hasShip) &&
-        (gameboard.board[shot[1] - 1][shot[0]].isHit && gameboard.board[shot[1] - 1][shot[0]].hasShip) &&
-        (!gameboard.board[shot[1] + 1 ][shot[0]].isHit)) {
-         shot[1] = shot[1] + 1;
-    } */
+    if (shot[1] + 1 < 10 && gameboard.board[shot[1] + 1][shot[0]].isHit && gameboard.board[shot[1] + 1][shot[0]].hasShip) {
+        if (shot[1] - 1 >= 0 && !gameboard.board[shot[1] - 1][shot[0]].isHit) {
+        gameboard.recieveAttack(shot[0], shot[1] - 1); return [shot[0], shot[1] - 1];
+        }
+    }
+    if (gameboard.board[shot[1] - 1][shot[0]] && gameboard.board[shot[1] - 1][shot[0]].isHit && gameboard.board[shot[1] - 1][shot[0]].hasShip) {
+        if (shot[1] + 1 < 10 && !gameboard.board[shot[1] + 1][shot[0]].isHit) {
+        gameboard.recieveAttack(shot[0], shot[1] + 1); return [shot[0], shot[1] + 1];
+        }
+    }
     
     // Horizontal Ship
+    if (gameboard.board[shot[1]][shot[0] - 1].isHit && gameboard.board[shot[1]][shot[0] + 1].isHit) {
+        const index = validSpots.indexOf(shot);
+        let newValid = validSpots.splice(index, 1);
+        shot = newValid[0];
+    }
     if (shot[0] + 1 < 10 && gameboard.board[shot[1]][shot[0] + 1].isHit && gameboard.board[shot[1]][shot[0] + 1].hasShip) {
-        if (shot[0] + 2 < 10 && shot[0] - 1 >= 0 && gameboard.board[shot[1]][shot[0] + 2].isHit) {
-            gameboard.recieveAttack(shot[0] - 1, shot[1]); return [shot[0] - 1, shot[1]];
+        if (shot[0] - 1 >= 0 && !gameboard.board[shot[1]][shot[0] - 1].isHit) {
+        gameboard.recieveAttack(shot[0] - 1, shot[1]); return [shot[0] - 1, shot[1]];
         }
-        gameboard.recieveAttack(shot[0] + 2, shot[1]); return [shot[0] + 2, shot[1]];
     }
     if (gameboard.board[shot[1]][shot[0] - 1] && gameboard.board[shot[1]][shot[0] - 1].isHit && gameboard.board[shot[1]][shot[0] - 1].hasShip) {
-        if (shot[0] + 1 < 10 && shot[0] + 2 < 10 && gameboard.board[shot[1]][shot[0] + 1].isHit) {
-            gameboard.recieveAttack(shot[0] + 2, shot[1]); return [shot[0] + 2, shot[1]];
-        }
+        if (shot[0] + 1 < 10 && !gameboard.board[shot[1]][shot[0] + 1].isHit) {
         gameboard.recieveAttack(shot[0] + 1, shot[1]); return [shot[0] + 1, shot[1]];
+        }
     }
     return false;
 }
